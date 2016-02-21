140dc73e68c6bae59847f0043e51d1e9418e0498
Ben van Klinken <ustramooner@users.sourceforge.net>
2016-02-21 11:40:52 +1000

    various debian packaging fixes

--- build.sh
+++ build.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
+#copy speach librayr in - otherwise first build fails
+mkdir bin/Release/ ; cp ./ExtLibs/System.Speech.dll bin/Release/System.Speech.dll
+
+#build with xbuild
 xbuild /property:DefineConstants=SQLite,MONO,noDIRECTX /p:Configuration=Release ArdupilotMega.sln
 
